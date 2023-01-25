54332b67ebcd5a79bb0a2bc66711540f20d90924
skova-yash <123085149+skova-yash@users.noreply.github.com>
2023-01-25 13:52:47 +0900

    3章 FizzBuzz問題の提出
    FizzBuzz.phpの作成

---
+++ 3-1/FizzBuzz.php
@@ -0,0 +1,17 @@
+<?php
+    for ($num = 1; $num <= 100; $num++) {
+        if ($num % 3 == 0 && $num % 5 == 0) {
+            echo "FizzBuzz!!";
+        } elseif ($num % 3 == 0) {
+            echo "Fizz!";
+        } elseif ($num % 5 == 0) {
+            echo "Buzz!";
+        } else {
+            echo $num;
+        }
+        
+        if ($num != 100) {
+            echo "<br>";
+        }
+    }
+?>
