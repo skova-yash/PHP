c94836012b515da7ea6b8515e448c5e91161bc8b
skova-yash <123085149+skova-yash@users.noreply.github.com>
2023-02-10 20:16:47 +0900

    4章 バグ修正を行うの提出
    以下の3ファイルの作成
    test1\index.php
    test2\dbconnect.php
    test2\index.php

---
+++ 4-1/bugTest/test2/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+// セッション開始
+//session_start();
+
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "";
+// データベース名
+//$db['dbname'] = "loginManagement";
+$db['dbname'] = "YIGroupBlog";
